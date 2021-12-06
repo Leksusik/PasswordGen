8e37956633ba7dc28e63fb1c7800caaf52006471
Mateusz Grzenia <93188703+Leksusik@users.noreply.github.com>
2021-12-06 22:03:16 +0100

    Update Password.java

--- PwdGen/src/main/java/com/casado/pwdgen/Password.java
+++ PwdGen/src/main/java/com/casado/pwdgen/Password.java
@@ -1,16 +1,7 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.casado.pwdgen;
 
 import java.security.SecureRandom;
 
-/**
- *
- * @author podiaserpior
- */
 public class Password {
     public static String generateRandomPassword(int len, String finalCharset)
     {
